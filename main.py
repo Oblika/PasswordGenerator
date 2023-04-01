5b1ce8af502ee8a8a9e0885b8e353223af867fd1
Samuel Oblika <samueloblika10@gmail.com>
2023-04-01 14:40:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import string
+import random
+
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_password():
+    password_length = int(input("How long would you like your password to be? "))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+       password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    password = "".join(password)
+
+    print(password)
+
+option = input("Do you wwant to generate a password (Yes/No): ")
+
+if option == "Yes":
+    generate_password()
+elif option == "No":
+    print("Progrma ended")
+    quit()
+else:
+    print("Invalid input, pleasy inpute Yes or No ")
+    quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
